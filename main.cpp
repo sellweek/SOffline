2f04c0e2274090f6be87b8c150aba64f58eb19f4
Róbert Selvek <robert@selvek.me>
2017-04-16 19:25:25 +0200

    Update the test script

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdint>
+#include <chrono>
+#include <string>
 
 #include "src/sqlite/sqlite_client.h"
 #include "src/sqlite/sqlite_statement.h"
@@ -8,7 +10,10 @@ int main() {
     std::cout << "Starting" << std::endl;
     sqlite::Client cl {"/Users/selvek/Dev/fit/PA2/semestralka/db.sqlite"};
     std::cout << "Database open" << std::endl;
-    sqlite::Statement insert {cl, "INSERT INTO test(name) VALUES ('Robert');"};
+    sqlite::Statement insert {cl, "INSERT INTO test(name) VALUES (?);"};
+    auto value = std::to_string(std::chrono::system_clock::now().time_since_epoch().count());
+    std::cout << "Will insert " << value << std::endl;
+    insert.bind(1, value);
     std::cout << "Statement created" << std::endl;
     bool result {insert.step()};
     std::cout << "Insert succeeded: " << !result << std::endl;
